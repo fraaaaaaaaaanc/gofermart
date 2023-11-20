364700937c22b639d4c5214adc0576acc0ffd58e
flyalwayswannatofly <frantsev_94@mail.ru>
2023-11-21 00:34:09 +0300

    fixed data in get_orders

--- internal/models/handlersmodels/get_orders.go
+++ internal/models/handlersmodels/get_orders.go
@@ -7,8 +7,8 @@ import (
 var ErrTheAreNoOrders = errors.New("there are no orders for this user")
 
 type RespGetOrders struct {
-	OrderNumber string `json:"number"`
-	Status      string `json:"status"`
-	Accrual     int    `json:"accrual,omitempty"`
-	UploadedAt  string `json:"uploaded_at"`
+	OrderNumber string  `json:"number"`
+	Status      string  `json:"status"`
+	Accrual     float64 `json:"accrual,omitempty"`
+	UploadedAt  string  `json:"uploaded_at"`
 }
