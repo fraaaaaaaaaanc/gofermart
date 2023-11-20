be0980336953a985b86fe702236ed53e4cc03f2c
flyalwayswannatofly <frantsev_94@mail.ru>
2023-11-21 00:39:19 +0300

    fixed data in withdrawals

--- internal/models/handlersmodels/withdrawals.go
+++ internal/models/handlersmodels/withdrawals.go
@@ -2,14 +2,13 @@ package handlersmodels
 
 import (
 	"errors"
-	"github.com/shopspring/decimal"
 	"time"
 )
 
 var ErrTheAreNoWithdraw = errors.New("there are no withdraw for this user")
 
 type RespWithdrawalsHistory struct {
-	OrderNumber string          `json:"number"`
-	SumWithdraw decimal.Decimal `json:"sum"`
-	ProcessedAt time.Time       `json:"processed_at"`
+	OrderNumber string    `json:"number"`
+	SumWithdraw float64   `json:"sum"`
+	ProcessedAt time.Time `json:"processed_at"`
 }
