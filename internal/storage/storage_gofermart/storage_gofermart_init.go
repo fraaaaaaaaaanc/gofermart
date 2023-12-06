4ce608a8d333cf2ac5fca02dd011f34995b5ceb3
flyalwayswannatofly <frantsev_94@mail.ru>
2023-12-06 14:46:11 +0300

    removed the use of migrations

--- internal/storage/storage_gofermart/storage_gofermart_init.go
+++ internal/storage/storage_gofermart/storage_gofermart_init.go
@@ -3,9 +3,7 @@ package storagegofermart
 import (
 	"context"
 	"database/sql"
-	"errors"
 	_ "github.com/jackc/pgx/v5/stdlib"
-	"github.com/pressly/goose"
 	"go.uber.org/zap"
 	"gofermart/internal/logger"
 )
@@ -37,75 +35,75 @@ func NewStorage(storageDBAddress string) (*Storage, error) {
 	//if err != nil {
 	//	return nil, err
 	//}
+	//
+	//err = goose.Up(db, migrationsDir)
+	//if err != nil && !errors.Is(err, goose.ErrNoNextVersion) {
+	//	logger.Error("error migration execution", zap.Error(err))
+	//	return nil, err
+	//}
 
-	err = goose.Up(db, migrationsDir)
-	if err != nil && !errors.Is(err, goose.ErrNoNextVersion) {
-		logger.Error("error migration execution", zap.Error(err))
-		return nil, err
-	}
-
-	//_, err = db.ExecContext(ctx, `
-	//DO $$
-	//BEGIN
-	//  IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'users') THEN
-	//      CREATE TABLE users (
-	//          id SERIAL PRIMARY KEY,
-	//          user_name VARCHAR(50) NOT NULL DEFAULT '' UNIQUE,
-	//          password VARCHAR(60) NOT NULL DEFAULT ''
-	//      );
-	//  END IF;
-	//
-	//  IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'orders') THEN
-	//      CREATE TABLE orders (
-	//          id SERIAL PRIMARY KEY,
-	//          user_id INT REFERENCES users(id),
-	//          order_number VARCHAR(32) NOT NULL DEFAULT '' UNIQUE,
-	//          order_status VARCHAR(10) NOT NULL DEFAULT '',
-	//          accrual DECIMAL NOT NULL DEFAULT 0,
-	//          order_datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-	//      );
-	//	END IF;
-	//
-	//	IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'orders_info') THEN
-	//      CREATE TABLE orders_info (
-	//          id SERIAL PRIMARY KEY,
-	//          order_id INT REFERENCES orders(id),
-	//          description VARCHAR(128) NOT NULL DEFAULT '',
-	//          price DECIMAL NOT NULL DEFAULT 0
-	//      );
-	//	END IF;
-	//
-	//	IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'balance') THEN
-	//	   CREATE TABLE balance (
-	//	       id SERIAL PRIMARY KEY,
-	//	       user_id INT REFERENCES users(id),
-	//	       user_balance DECIMAL NOT NULL DEFAULT 0 CHECK (user_balance >= 0),
-	//	       withdrawn_balance DECIMAL NOT NULL DEFAULT 0
-	//	    );
-	//	END IF;
-	//
-	//	IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'history_balance') THEN
-	//	   CREATE TABLE history_balance (
-	//	       id SERIAL PRIMARY KEY,
-	//	       user_id INT REFERENCES users(id),
-	//	       order_number_unregister VARCHAR(32) NOT NULL DEFAULT '' UNIQUE,
-	//	       withdrawn_sum DECIMAL NOT NULL DEFAULT 0 CHECK (withdrawn_sum >= 0),
-	//	       withdrawn_datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-	//	   );
-	//  END IF;
-	//
-	//  IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'order_accrual') THEN
-	//	   CREATE TABLE order_accrual (
-	//	       id SERIAL PRIMARY KEY,
-	//	       user_id INT REFERENCES users(id),
-	//	       order_id INT REFERENCES orders(id),
-	//	       order_number VARCHAR(32) NOT NULL DEFAULT '' UNIQUE,
-	//	       order_status_accrual VARCHAR(10) NOT NULL DEFAULT '',
-	//	       accrual DECIMAL NOT NULL DEFAULT 0
-	//	   );
-	//  END IF;
-	//END $$;
-	//`)
+	_, err = db.ExecContext(ctx, `
+	DO $$
+	BEGIN
+	 IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'users') THEN
+	     CREATE TABLE users (
+	         id SERIAL PRIMARY KEY,
+	         user_name VARCHAR(50) NOT NULL DEFAULT '' UNIQUE,
+	         password VARCHAR(60) NOT NULL DEFAULT ''
+	     );
+	 END IF;
+	
+	 IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'orders') THEN
+	     CREATE TABLE orders (
+	         id SERIAL PRIMARY KEY,
+	         user_id INT REFERENCES users(id),
+	         order_number VARCHAR(32) NOT NULL DEFAULT '' UNIQUE,
+	         order_status VARCHAR(10) NOT NULL DEFAULT '',
+	         accrual DECIMAL NOT NULL DEFAULT 0,
+	         order_datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+	     );
+		END IF;
+	
+		IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'orders_info') THEN
+	     CREATE TABLE orders_info (
+	         id SERIAL PRIMARY KEY,
+	         order_id INT REFERENCES orders(id),
+	         description VARCHAR(128) NOT NULL DEFAULT '',
+	         price DECIMAL NOT NULL DEFAULT 0
+	     );
+		END IF;
+	
+		IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'balance') THEN
+		   CREATE TABLE balance (
+		       id SERIAL PRIMARY KEY,
+		       user_id INT REFERENCES users(id),
+		       user_balance DECIMAL NOT NULL DEFAULT 0 CHECK (user_balance >= 0),
+		       withdrawn_balance DECIMAL NOT NULL DEFAULT 0
+		    );
+		END IF;
+	
+		IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'history_balance') THEN
+		   CREATE TABLE history_balance (
+		       id SERIAL PRIMARY KEY,
+		       user_id INT REFERENCES users(id),
+		       order_number_unregister VARCHAR(32) NOT NULL DEFAULT '' UNIQUE,
+		       withdrawn_sum DECIMAL NOT NULL DEFAULT 0 CHECK (withdrawn_sum >= 0),
+		       withdrawn_datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+		   );
+	 END IF;
+	
+	 IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'order_accrual') THEN
+		   CREATE TABLE order_accrual (
+		       id SERIAL PRIMARY KEY,
+		       user_id INT REFERENCES users(id),
+		       order_id INT REFERENCES orders(id),
+		       order_number VARCHAR(32) NOT NULL DEFAULT '' UNIQUE,
+		       order_status_accrual VARCHAR(10) NOT NULL DEFAULT '',
+		       accrual DECIMAL NOT NULL DEFAULT 0
+		   );
+	 END IF;
+	END $$;
+	`)
 
 	if err != nil {
 		return nil, err
